449a627d53d560e8234d55978b4e19fd02e57f33
joseoscarcc <94883708+joseoscarcc@users.noreply.github.com>
2023-10-31 19:52:00 -0600

    Update PublishLogic.cs
    ABLE TO CONVERT THE DATALOGGER INTO A ORGANIZE AND WELL STRUCTURED JSON

--- PublishLogic.cs
+++ PublishLogic.cs
@@ -33,22 +33,23 @@ public class PublicLogic : BaseNetLogic
     private PeriodicTask MiTask;
     public override void Start()
     {
-         // Configure MQTT credentials
+        // Configure MQTT credentials
         var broker = LogicObject.GetVariable("Broker");
         string brokerIpAddress = broker.Value;
-        var port = LogicObject.GetVariable("Port");
-        var clientID = LogicObject.GetVariable("ClientID");
-        var username = LogicObject.GetVariable("Username");
-        var password = LogicObject.GetVariable("Broker");
+        int port = LogicObject.GetVariable("Port").Value;
+        string clientID = LogicObject.GetVariable("ClientID").Value;
+        string username = LogicObject.GetVariable("Username").Value;
+        string password = LogicObject.GetVariable("Password").Value;
 
-        //publishClient = new MqttClient(brokerIpAddress, 1883, true, null, null, MqttSslProtocols.TLSv1_2);
-        publishClient = new MqttClient(brokerIpAddress);
-       
-        publishClient.Connect(clientID.Value);
+        publishClient = new MqttClient(brokerIpAddress, port, true, null, null, MqttSslProtocols.TLSv1_2);
+        //publishClient = new MqttClient(brokerIpAddress);
+
+        // Connect to the broker
+        publishClient.Connect(clientID, username, password, false, 60);
         // Assign a callback to be executed when a message is published to the broker
         publishClient.MqttMsgPublished += PublishClientMqttMsgPublished;
 
-        
+
         MiTask = new PeriodicTask(PublishMessage, 250, LogicObject);
         MiTask.Start();
 
@@ -70,34 +71,53 @@ public class PublicLogic : BaseNetLogic
         var dataLoggerStore = LogicObject.GetVariable("DataLogger").Value;
         var dataInfo = InformationModel.Get<DataLogger>(dataLoggerStore);
 
-        // Log.Info(dataInfo.TableName);
-    
-        // foreach (var variableToLog in dataInfo.VariablesToLog.ToList())
-        // {
-        //     Log.Info(variableToLog.BrowseName);
-        //     Log.Info(variableToLog.Value);
-        // }
+        Dictionary<string, Dictionary<string, string>> dictOfDicts = new Dictionary<string, Dictionary<string, string>>();
+        Dictionary<string, string> currentDict = null;
+        string lastAssetName = null;
 
-        var dataToSerialize = new {
-            TableName = dataInfo.TableName,
-            VariablesToLog = dataInfo.VariablesToLog.Select(v => new {
-                BrowseName = v.BrowseName,
-                Value = v.Value
-            })
-        };
+        foreach (var variableToLog in dataInfo.VariablesToLog.ToList())
+        {
+            string[] parts = variableToLog.BrowseName.Split('_');
+            if (parts.Length < 3)
+            {
+                if (lastAssetName != parts[0])
+                {
+                    // New asset name, create a new dictionary
+                    currentDict = new Dictionary<string, string>();
+                    dictOfDicts[parts[0]] = currentDict;
+                    lastAssetName = parts[0];
+                }
+                // Add the variable to the current dictionary
+                currentDict[parts[1]] = variableToLog.Value;
+            } 
+            else if (parts.Length > 2)
+            {
+                if (parts[1] == "Asset" || parts[1] == "High")
+                {
+                    if (lastAssetName == parts[0])
+                    {
+                        currentDict[parts[1] + "_" + parts[2]] = variableToLog.Value;
+                    }
+                }
+            }
+            // Add timestamp to the current dictionary
+            currentDict["Timestamp"] = DateTime.Now.ToString();
+        }
 
-        var json = JsonConvert.SerializeObject(dataToSerialize);
+        string json = JsonConvert.SerializeObject(dictOfDicts, Formatting.Indented);
+        Log.Info(json);
         var topic = LogicObject.GetVariable("Topic");
 
         ushort msgId = publishClient.Publish(topic.Value, // topic
             System.Text.Encoding.UTF8.GetBytes(json), // message body
             MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, // QoS level
             false); // retained
-        
+
     }
+    
 
 
     private MqttClient publishClient;
 
-    
+
 }
